33ce48d8060838ab81bd08643caa7be320a46d9b
Alena Shurakova <alena.shurakova@intapp.com>
2023-07-30 22:21:35 +0300

    Add project files.

---
+++ AqaLabHomeworkFilesStorage/Task8/ExceptionsHandling.cs
@@ -0,0 +1,18 @@
+﻿namespace AqaLabHomeworkFilesStorage.Task8
+{
+    public class ExceptionsHandling
+    {
+        public static void ShowMassiveElement()
+        {
+            var massive = new int[] { 8, 7, 1, 4, 2 };
+            Console.WriteLine("Input index of element in massive:");
+            var inputedValue = Console.ReadLine();
+
+            var elementIndex = inputedValue.Equals(string.Empty) ? null : inputedValue;
+            int parsedelementIndex = int.Parse(elementIndex);
+            int elementOfMassiveByIndex = massive[parsedelementIndex];
+
+            Console.WriteLine($"Massive element that has index {inputedValue} has value {elementOfMassiveByIndex}");
+        }
+    }
+}
